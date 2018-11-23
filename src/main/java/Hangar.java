a1b95061a592cb5d647585fa43beb2016ee4b730
Graham-Stein <graham@guavoaviation.co.uk>
2018-11-23 15:14:23 +0000

    Add hangar progress

--- src/main/java/Hangar.java
+++ src/main/java/Hangar.java
@@ -1,2 +1,18 @@
 public class Hangar {
+
+    private int aircraftParkingSpaces;
+    private String hangarName;
+
+    public Hangar(String hangarName, int aircraftParkingSpaces){
+        this.hangarName = hangarName;
+        this.aircraftParkingSpaces = aircraftParkingSpaces;
+    }
+
+    public int getAircraftParkingSpaces(){
+        return this.aircraftParkingSpaces;
+    }
+
+    public String getHangarName(){
+        return this.hangarName;
+    }
 }
--- src/test/java/HangarTest.java
+++ src/test/java/HangarTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HangarTest {
+
+    private Hangar hangar;
+    private Hangar hangar1;
+
+    @Before
+    public void before(){
+        hangar = new Hangar("Virkie", 5);
+        hangar1 = new Hangar("Sella Ness", 4);
+    }
+
+    @Test
+    public void canGetNumberOfParkingSpacesInHangar(){
+        assertEquals(5, hangar.getAircraftParkingSpaces());
+    }
+
+    @Test
+    public void canGetHangarName(){
+        assertEquals("Sella Ness", hangar1.getHangarName());
+    }
 }
